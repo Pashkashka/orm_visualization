bd3d8a3b85ea219ced5169db4d94c70f1f96e56f
Pashkashka <ovsyannikovpv@innovaspectr.ru>
2024-11-22 00:13:10 +0300

    fix4

--- src/pages/albums/index.jsx
+++ src/pages/albums/index.jsx
@@ -534,7 +534,7 @@ function AlbumPage() {
   const [settingsOpen, setSettingsOpen] = useState(false);
   return (
     <div className={styles.contaner}>
-      <Header title="Список студентов" />
+      <Header title="Список студентоввввы" />
       <div className={styles.filters}>
         <div className={styles.search}>
           <SearchInput
