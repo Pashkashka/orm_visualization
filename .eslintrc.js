0f903450f83d21327e52ce14ae849943613320dd
Pashkashka <ovsyannikovpv@innovaspectr.ru>
2024-11-22 00:43:20 +0300

    fix7

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
     es2021: true,
     node: true, // Добавлено для NODE окружения
   },
+  parserOptions: {
+    ecmaVersion: "latest", // Или конкретная версия ECMAScript, например, 2020
+    sourceType: "module", // Укажите, что файлы являются модулями
+  },
   rules: {
     // ... другие правила ...
     "no-unused-vars": process.env.NODE_ENV === "production" ? "warn" : "warn",
